2ad08f6a783ac9781e1e6b3b862d11a5257110f7
Tim G. Thomas <tim@timgthomas.com>
2016-06-25 20:48:18 -0500

    show currently-playing sounds

--- app/pods/components/sound-list/component.js
+++ app/pods/components/sound-list/component.js
@@ -3,6 +3,7 @@ import groupBy from 'ember-group-by';
 
 export default Ember.Component.extend({
 
-  categories: groupBy('sounds', 'category')
+  categories: groupBy('sounds', 'category'),
+  playingSounds: Ember.computed.filterBy('sounds', 'playing')
 
 });
