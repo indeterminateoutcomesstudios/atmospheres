46de46ae4ee2a7bb343157f51110203a654ed9e8
Tim G. Thomas <tim@timgthomas.com>
2016-06-25 22:50:08 -0500

    rearrange sound lists

--- app/services/sounds.js
+++ app/services/sounds.js
@@ -14,10 +14,13 @@ export default Ember.Service.extend({
       { name: 'Rain (Medium)',  category: 'Weather', url: 'weather/rain-medium.wav' },
       { name: 'Rain (Heavy)',   category: 'Weather', url: 'weather/rain-heavy.wav' },
 
-      { name: 'Treasure Goblin', url: 'treasure goblin.mp3' },
-      { name: 'Waterfall (Small)' },
-      { name: 'Waterfall (Medium)' },
-      { name: 'Waterfall (Large)' }
+      // Water
+      { name: 'Brook',           category: 'Water', url: 'water/brook.wav' },
+      { name: 'Stream',          category: 'Water', url: 'water/stream.wav' },
+      { name: 'River',           category: 'Water', url: 'water/river.wav' },
+      { name: 'Waves (Shallow)', category: 'Water', url: 'water/waves-shallow.wav' },
+      { name: 'Waves (Medium)',  category: 'Water', url: 'water/waves-medium.wav' },
+      { name: 'Waves (Deep)',    category: 'Water', url: 'water/waves-deep.wav' }
 
     ];
     return Ember.A(soundStubs.map(s => Sound.create({ ...s, context: this.get('player.context') })));
