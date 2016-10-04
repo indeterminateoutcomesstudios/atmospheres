0baa8a3ef40ed1519903516b38aad4f34ef51320
Tim G. Thomas <tim@timgthomas.com>
2016-10-03 21:29:00 -0500

    refactor sound lists into a component

--- app/pods/components/main-controls/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-  player: Ember.inject.service(),
-
-  playingSounds: Ember.computed.filterBy('sounds', 'playing')
-
-});
--- app/pods/components/sound-list/component.js
+++ app/pods/components/sound-list/component.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
-import groupBy from 'ember-group-by';
 
 export default Ember.Component.extend({
 
-  categories: groupBy('sounds', 'category'),
-  playingSounds: Ember.computed.filterBy('sounds', 'playing')
+  tagName: 'ul'
 
 });
